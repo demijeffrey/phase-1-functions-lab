8b74eb120af37c4587cf4e0238671434626b88b3
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-19 13:18:49 -0600

    commit tree 459f798ab7657a137eb3c8dce83baef40295a45b

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(destination, hq=42) {
+    if (destination > hq) {
+    return destination - hq
+    } else {
+        return hq - destination
+    }
+}
+
+function distanceFromHqInFeet(destination, hq) {
+    distanceFromHqInBlocks(destination, hq);
+    return distanceFromHqInBlocks(destination, hq) * 264
+}
+
+function distanceTravelledInFeet (start, destination) {
+    function blocks(start, destination) {
+        if (start > destination) {
+            return start - destination
+        } else if (start < destination) {
+            return destination - start
+        }
+    }
+    return blocks(start, destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination)
+    if (feet < 400) {
+        return 0
+    } else if (feet > 400 && feet <=2000) {
+        return (feet-400) * 0.02
+    } else if (feet > 2000 && feet < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
